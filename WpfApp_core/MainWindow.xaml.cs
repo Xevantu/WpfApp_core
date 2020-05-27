c8b8ec5b4724c84c751b77de75ad4901154ea841
至軒 李 <ivan.lee@realtek.com>
2020-05-27 13:26:23 +0800

    '新增排版方式，將封面、名稱、介紹，列表至左方，

--- WpfApp_core/MainWindow.xaml.cs
+++ WpfApp_core/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using HtmlAgilityPack;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Net;
+using System.Security.Cryptography.Xml;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -14,15 +17,29 @@ namespace WpfApp_core
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        Size_m CoverSize = new Size_m
+        {
+            Height = 150,
+            Width = 100
+        };
+
+        Size_m IntroSize = new Size_m
+        {
+            Height = 100,
+            Width = 300
+        };
         public MainWindow()
         {
             InitializeComponent();
+            
             #region
-            Grid mGrid = new Grid();
-            Label lb_html = new Label();
-            Image img = new Image();
+            List<Grid> List_manhuaGrid = new List<Grid> { };
+            
+            string strManHuaName, strManHuaIntro;
+            
             #endregion
             Canvas canvas = new Canvas();
             
@@ -39,41 +56,33 @@ namespace WpfApp_core
              * 註: 回傳為list之後起始位置=0, 尚未回傳要直接指向的時候起始位置=1
              */
             HtmlNodeCollection list = doc.DocumentNode.SelectNodes("/html/body/ul/li");
-            //標籤img內有需要的網址，透過Attribute取出
-            string Url = list[0].SelectSingleNode("div[1]/a/img").Attributes["src"].Value;
-            //將網址字串轉為Uri格式
-            Uri uri = new Uri(Url);
-            //透過網址下載圖片轉為BitmapImage格式
-            BitmapImage image = new BitmapImage(uri);
-            //使用Image儲存該BitmapImage
-            img.Source = image;
-
-            string name = list[0].SelectSingleNode("div[2]/a/p[1]").InnerText;
-            string introduction = list[0].SelectSingleNode("div[2]/a/p[2]").InnerText;
-            lb_html.Content = "URL: " + Url +
-                "\nName: " + name + "\n介紹: " + introduction;
-            //設置元件位置
-            img.HorizontalAlignment = HorizontalAlignment.Left;
-            img.VerticalAlignment = VerticalAlignment.Center;
-            img.Margin = new Thickness(2);
-            img.Width = 100;
-            img.Height = 150;
-            lb_html.HorizontalAlignment = HorizontalAlignment.Left;
-            lb_html.VerticalAlignment = VerticalAlignment.Center;
-            lb_html.HorizontalContentAlignment = HorizontalAlignment.Left;
-            lb_html.VerticalContentAlignment = VerticalAlignment.Center;
-            lb_html.Margin = new Thickness(img.Margin.Left + img.Width, img.Margin.Top, 0, 0);
-            lb_html.Width = 300;
-            lb_html.Height = 100;
-            mGrid.HorizontalAlignment = HorizontalAlignment.Left;
-            mGrid.VerticalAlignment = VerticalAlignment.Top;
-            mGrid.Width = img.Width + lb_html.Width;
-            mGrid.Height = img.Height;
-            mGrid.Background = System.Windows.Media.Brushes.Red;
-            mGrid.Children.Add(img);
-            mGrid.Children.Add(lb_html);
-
-            main_bg.Children.Add(mGrid);
+            //
+            for (int cnt = 0; cnt < list.Count; cnt++)
+            {
+                //標籤img內有需要的網址，透過Attribute取出
+                string Url = list[cnt].SelectSingleNode("div[1]/a/img").Attributes["src"].Value;
+                //將網址字串轉為Uri格式
+                Uri uri = new Uri(Url);
+                //透過網址下載圖片轉為BitmapImage格式
+                BitmapImage image = new BitmapImage(uri);
+                /* 使用Image儲存該BitmapImage
+                 * 註: 透過引數傳送指向的Image會是同一個位址，無法使用同一個Image來複寫傳送圖像資料，必須將每一筆圖像都分開建立成獨立檔案。
+                */
+                Image img = new Image
+                {
+                    Source = image
+                };
+                Thread.Sleep(5);
+                strManHuaName = list[cnt].SelectSingleNode("div[2]/a/p[1]").InnerText;
+                strManHuaIntro = list[cnt].SelectSingleNode("div[2]/a/p[2]").InnerText;
+                List_manhuaGrid.Add(Typesetting(-1, img, strManHuaName, strManHuaIntro));
+            }
+            
+            for (int i = 0; i < List_manhuaGrid.Count; i++)
+            {
+                List_manhuaGrid[i].Margin = new Thickness(5, 5 + CoverSize.Height * i, 0, 0);
+                main_bg.Children.Insert(0, List_manhuaGrid[i]);
+            }
             
         }
 
@@ -96,5 +105,58 @@ namespace WpfApp_core
             return res;
         }
 
+        struct LayoutGrid
+        {
+            #region 佈局樣本
+            public Image CoverImage { get => CoverImage; set => CoverImage = value; }
+            public Label ContentLabel { get => ContentLabel; set => ContentLabel = value; }
+            public Grid BlockedGrid { get => BlockedGrid; set => BlockedGrid = value; }
+            #endregion
+        };
+
+        struct Size_m
+        {
+            public double Height { get; set; }
+            public double Width { get; set; }
+            public double Length { get; set; }
+            public double SerialNember { get; set; }
+        }
+
+        public Grid Typesetting(int mode, Image rImg, string name, string introduction)
+        {
+            Grid grid = new Grid();
+            Label label = new Label();
+            Image img = rImg;
+            switch (mode)
+            {
+                case 0:
+                    break;
+                default:
+                    label.Content = name + "\n" + introduction;
+                    //設置元件位置
+                    img.HorizontalAlignment = HorizontalAlignment.Left;
+                    img.VerticalAlignment = VerticalAlignment.Center;
+                    img.Margin = new Thickness(2);
+                    img.Width = CoverSize.Width;
+                    img.Height = CoverSize.Height;
+                    label.HorizontalAlignment = HorizontalAlignment.Left;
+                    label.VerticalAlignment = VerticalAlignment.Center;
+                    label.HorizontalContentAlignment = HorizontalAlignment.Left;
+                    label.VerticalContentAlignment = VerticalAlignment.Center;
+                    label.Margin = new Thickness(img.Margin.Left + img.Width, img.Margin.Top, 0, 0);
+                    label.Width = IntroSize.Width;
+                    label.Height = IntroSize.Height;
+                    grid.HorizontalAlignment = HorizontalAlignment.Left;
+                    grid.VerticalAlignment = VerticalAlignment.Top;
+                    grid.Width = img.Width + label.Width;
+                    grid.Height = img.Height;
+                    grid.Background = System.Windows.Media.Brushes.Red;
+                    grid.Children.Add(img);
+                    grid.Children.Add(label);
+                    break;
+            }
+            return grid;
+        }
     }
+    
 }
