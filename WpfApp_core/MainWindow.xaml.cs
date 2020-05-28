c66f191bbd2b792f73c02a1120f33c9cfa50088b
至軒 李 <ivan.lee@realtek.com>
2020-05-28 16:50:48 +0800

    新增ListBox放搜尋結果並可滾動

--- WpfApp_core/MainWindow.xaml.cs
+++ WpfApp_core/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography.Xml;
 using System.Threading;
 using System.Windows;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,35 +23,41 @@ namespace WpfApp_core
     
     public partial class MainWindow : Window
     {
-        Size_m CoverSize = new Size_m
+        public struct Size_m
+        {
+            public double Height { get; set; }
+            public double Width { get; set; }
+            public double Length { get; set; }
+            public double SerialNember { get; set; }
+        }
+
+        readonly static Size_m CoverSize = new Size_m
         {
             Height = 150,
             Width = 100
         };
-
-        Size_m IntroSize = new Size_m
+        readonly static Size_m IntroSize = new Size_m
         {
             Height = 100,
             Width = 300
-        };
+        };      
         public MainWindow()
         {
             InitializeComponent();
-            
-            #region
+            #region 搜尋結果清單
             List<Grid> List_manhuaGrid = new List<Grid> { };
-            
             string strManHuaName, strManHuaIntro;
-            
             #endregion
-            Canvas canvas = new Canvas();
-            
-            HtmlWeb webClient = new HtmlWeb(); //建立htmlweb
+
+            //建立htmlweb
+            HtmlWeb webClient = new HtmlWeb(); 
             //處理C# 連線 HTTPS 網站發生驗證失敗導致基礎連接已關閉
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls |
             SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
-            HtmlDocument doc = webClient.Load("https://www.manhuaren.com/search?title=%E8%BC%9D%E5%A4%9Ca%E6%97%A5&language=1"); //載入網址資料
+            #region 搜尋動作及結果
+            //載入網址資料
+            HtmlDocument doc = webClient.Load("https://www.manhuaren.com/search?title=%E8%BC%9D%E5%A4%9Ca%E6%97%A5&language=1"); 
             //lb_html.Content = doc.Text;   //確認是否有抓到html代碼
 
             /* SelectNodes: 切換到指定的層
@@ -56,7 +65,7 @@ namespace WpfApp_core
              * 註: 回傳為list之後起始位置=0, 尚未回傳要直接指向的時候起始位置=1
              */
             HtmlNodeCollection list = doc.DocumentNode.SelectNodes("/html/body/ul/li");
-            //
+            //將掃到的內容全部排列後放到清單中
             for (int cnt = 0; cnt < list.Count; cnt++)
             {
                 //標籤img內有需要的網址，透過Attribute取出
@@ -75,29 +84,46 @@ namespace WpfApp_core
                 Thread.Sleep(5);
                 strManHuaName = list[cnt].SelectSingleNode("div[2]/a/p[1]").InnerText;
                 strManHuaIntro = list[cnt].SelectSingleNode("div[2]/a/p[2]").InnerText;
-                List_manhuaGrid.Add(Typesetting(-1, img, strManHuaName, strManHuaIntro));
+                List_manhuaGrid.Add(LayoutListSetting(-1, img, strManHuaName, strManHuaIntro));
             }
-            
+            //將漫畫清單放到卷軸中
             for (int i = 0; i < List_manhuaGrid.Count; i++)
             {
-                List_manhuaGrid[i].Margin = new Thickness(5, 5 + CoverSize.Height * i, 0, 0);
-                main_bg.Children.Insert(0, List_manhuaGrid[i]);
+                //List_manhuaGrid[i].Margin = new Thickness(0, 0, 0, 0.1);
+                
             }
             
+            //設定列表排版
+            ListBox_manhua.HorizontalAlignment = HorizontalAlignment.Left;
+            ListBox_manhua.Margin = new Thickness(0);
+            ListBox_manhua.Width = CoverSize.Width + IntroSize.Width;
+            //加入抓到的清單
+            ListBox_manhua.ItemsSource = List_manhuaGrid;
+            #endregion
         }
-
+        /// <summary>
+        /// 瀏覽網頁
+        /// </summary>
+        /// <param name="site">目標網頁網址</param>
         public void BrowseHttp(String site)
         {
             WebBrowser webBrowser = new WebBrowser();
             webBrowser.Source = new Uri(site);
             this.Content = webBrowser;
         }
-
+        /// <summary>
+        /// 搜尋漫畫
+        /// </summary>
+        /// <param name="manhuaming">搜尋漫畫名稱</param>
         public void SearchManHuaRen(string manhuaming)
         {
             BrowseHttp(ManHuaRenSearchFormat(manhuaming));
         }
-
+        /// <summary>
+        /// 漫畫人搜尋格式
+        /// </summary>
+        /// <param name="target">搜尋漫畫名稱</param>
+        /// <returns>回傳16進制網址字串</returns>
         private string ManHuaRenSearchFormat(string target)
         {
             string res = "https://www.manhuaren.com/search?title=" + target + "&language=1";
@@ -105,24 +131,7 @@ namespace WpfApp_core
             return res;
         }
 
-        struct LayoutGrid
-        {
-            #region 佈局樣本
-            public Image CoverImage { get => CoverImage; set => CoverImage = value; }
-            public Label ContentLabel { get => ContentLabel; set => ContentLabel = value; }
-            public Grid BlockedGrid { get => BlockedGrid; set => BlockedGrid = value; }
-            #endregion
-        };
-
-        struct Size_m
-        {
-            public double Height { get; set; }
-            public double Width { get; set; }
-            public double Length { get; set; }
-            public double SerialNember { get; set; }
-        }
-
-        public Grid Typesetting(int mode, Image rImg, string name, string introduction)
+        public Grid LayoutListSetting(int mode, Image rImg, string name, string introduction)
         {
             Grid grid = new Grid();
             Label label = new Label();
@@ -143,19 +152,27 @@ namespace WpfApp_core
                     label.VerticalAlignment = VerticalAlignment.Center;
                     label.HorizontalContentAlignment = HorizontalAlignment.Left;
                     label.VerticalContentAlignment = VerticalAlignment.Center;
+                    label.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromArgb(100, 255, 255, 255));
                     label.Margin = new Thickness(img.Margin.Left + img.Width, img.Margin.Top, 0, 0);
                     label.Width = IntroSize.Width;
                     label.Height = IntroSize.Height;
-                    grid.HorizontalAlignment = HorizontalAlignment.Left;
+                    grid.HorizontalAlignment = HorizontalAlignment.Right;
                     grid.VerticalAlignment = VerticalAlignment.Top;
+                    grid.Margin = new Thickness(-5);
                     grid.Width = img.Width + label.Width;
                     grid.Height = img.Height;
-                    grid.Background = System.Windows.Media.Brushes.Red;
+                    grid.Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb(100, 43, 43, 43));
                     grid.Children.Add(img);
                     grid.Children.Add(label);
                     break;
             }
             return grid;
+        }
+
+        private void Main_szChange(object sender, SizeChangedEventArgs e)
+        {
+            
+            ListBox_manhua.Height = main_bg.Height;
         }
     }
     
